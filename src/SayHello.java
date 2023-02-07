49eeee508f086e40f3ac1c9b999976f35efbf6d5
Carlos Emilio <22670017@iguala.tecnm.mx>
2023-02-07 07:41:41 -0600

    Creacion de la calse SayHello

---
+++ src/SayHello.java
@@ -0,0 +1,16 @@
+public class SayHello {
+    public static void main(String[] args) {
+
+        String name = "Carlos";
+
+        System.out.println("Hello World my name is "+ name);
+    }
+}
+/*git add * (nombre de archivo)
+
+git copmmit -m "mensaje"
+
+git push origin remote
+            |-----|
+          >Repositorios<
+ */
