6391666600e863aaabfd8102ad0f916253725a52
Carlos Emilio <22670017@iguala.tecnm.mx>
2023-02-14 07:18:29 -0600

    "Se crea la clase guiatarra"

---
+++ src/Guitar.java
@@ -0,0 +1,55 @@
+public class Guitar {
+
+
+    private String serialNnumber;
+    public String getSerialNnumber() {
+        return serialNnumber;
+
+    }public void setSerialNnumber(String serialNnumber) {
+        this.serialNnumber = serialNnumber;
+    }
+    private String price;
+    public String getPrice() {
+        return price;
+    }public void setPrice(String price) {
+        this.price = price;
+    }
+
+
+    private String builder;
+
+    public String getBuilder() {
+        return builder;
+    }public void setBuilder(String builder) {
+        this.builder = builder;
+    }
+
+    private String model;
+    public String getModel() {
+        return model;
+    }public void setModel(String model) {
+        this.model = model;
+    }
+
+    private String type;
+    public String getType() {
+        return type;
+    }public void setType(String type) {
+        this.type = type;
+    }
+
+    private String backWood;
+    public String getBackWood() {
+        return backWood;
+    }public void setBackWood(String backWood) {
+        this.backWood = backWood;
+    }
+
+    private String topWood;
+    public String getTopWood() {
+        return topWood;
+    }public void setTopWood(String topWood) {
+        this.topWood = topWood;
+    }
+
+}
